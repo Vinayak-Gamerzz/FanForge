233d8a6c047cbd71082f8f31270d83053143c64b
OG_Vinayak <vinayaktiwari307@gmail.com>
2026-08-29 23:04:19 +0530

    work done hell yeah

--- FAQ.js
+++ FAQ.js
@@ -5,3 +5,28 @@ faqs.forEach((faq) => {
         faq.classList.toggle("active");
     });
 });
+
+function showCategory(category) {
+
+    const sections = document.querySelectorAll(".faq-section");
+    const buttons = document.querySelectorAll(".faq-btn");
+
+    sections.forEach((section) => {
+
+        section.classList.remove("active");
+
+    });
+
+    buttons.forEach((button) => {
+
+        button.classList.remove("active");
+
+    });
+
+    document.getElementById(category).classList.add("active");
+
+    Event.currentTarget.classList.add("active");
+
+}
+
+document.getElementById("about").classList.add("active");
